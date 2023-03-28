1905a6b2b1d53e6f9453620561138c164c259432
Alexandr <67562340+The-Expert0@users.noreply.github.com>
2023-03-28 13:41:05 +0300

    Версия 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 from prettytable import PrettyTable
 # pip install PrettyTable   -для установки пакета
@@ -133,7 +134,6 @@ def create_db():
                                     login TEXT NOT NULL,
                                     password TEXT NOT NULL);'''
         cursor = sqlite_connection.cursor()
-        print("База данных подключена к SQLite")
         cursor.execute(sqlite_create_table_query)
         sqlite_connection.commit()
         print("Таблица SQLite создана")
@@ -146,32 +146,76 @@ def create_db():
             # print("Соединение с SQLite закрыто")
 
 
+def drop_table():
+    print("Таблица очищена.")
+    query = "DROP TABLE list_passwords;"
+    sql_commit(query)
+
+
 # create_db() # Создание БД и таблицы
-while True:
-    cmd = input(f"\n{'-' * 40}\n"
-                f"Выберете пункт меню введя цифру:\n"
-                f"1) Вывести все пароли;\n"
-                f"2) Вывести пароль по id;\n"
-                f"3) Поиск пароля по названию сайта;\n"
-                f"4) Добавить пароль;\n"
-                f"5) Изменить пароль;\n"
-                f"6) Удалить пароль;\n"
-                f"7) Закрыть программу.\n")
-    match cmd:
-        case "1":
-            select_all()
-        case "2":
-            select_id()
-        case "3":
-            select_website()
-        case "4":
-            insert()
-        case "5":
-            update()
-        case "6":
-            delete()
-        case "7":
-            print("Завершение работы.")
-            break
-        case _:
-            print("Неверная команда.")
+def main():
+    while True:
+        cmd = input(f"\n{'-' * 40}\n"
+                    f"Выберете пункт меню введя цифру:\n"
+                    f"1) Вывести все пароли;\n"
+                    f"2) Вывести пароль по id;\n"
+                    f"3) Поиск пароля по названию сайта;\n"
+                    f"4) Добавить пароль;\n"
+                    f"5) Изменить пароль;\n"
+                    f"6) Удалить пароль;\n"
+                    f"7) Закрыть программу.\n")
+        match cmd:
+            case "1":
+                select_all()
+            case "2":
+                select_id()
+            case "3":
+                select_website()
+            case "4":
+                insert()
+            case "5":
+                update()
+            case "6":
+                delete()
+            case "7":
+                print("Завершение работы.")
+                break
+            case _:
+                print("Неверная команда.")
+
+
+def verify():
+    """Функция ввода пароля при запуске программы"""
+    file_name = "pass.txt"
+    try:
+        file_ = open(file_name, 'r')
+        password = file_.read()    # Пароль - pass
+        file_.close()
+        pass_ = input(
+            "Введите пароль для доступа к программе: ").encode('utf-8')
+        if hashlib.sha224(pass_).hexdigest() == password:
+            print("Верный пароль.")
+            main()
+        else:
+            cmd = input("Неверный пароль. (1 - Попробовать еще раз, "
+                        "2 - Сбросить пароль и очистить БД) ")
+            match cmd:
+                case "1":
+                    print("*Пароль указан в функции verify*")
+                    verify()
+                case "2":
+                    new_pass = input("Введите новый пароль: ").encode('utf-8')
+                    file_ = open(file_name, 'w')
+                    file_.seek(0)
+                    file_.write(hashlib.sha224(new_pass).hexdigest())
+                    file_.close()
+                    drop_table()
+                    create_db()
+                    verify()
+    except FileNotFoundError:
+        print("Файл с паролем не был найден и был создан.")
+        file_ = open(file_name, 'w')
+        file_.close()
+
+
+verify()
